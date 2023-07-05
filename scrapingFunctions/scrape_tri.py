de0c0667117246e56a5259d260b8438a4b3bad9e
bagood <4riabagus@gmail.com>
2023-07-05 14:49:05 +0700

    updates the scraping program for xl and tri

--- scrapingFunctions/scrape_tri.py
+++ scrapingFunctions/scrape_tri.py
@@ -1,30 +1,29 @@
 import time
+import numpy as np
+import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_argument("start-maximized")
 webdriver_path = './chromedriver'
 
-driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
-main_url = 'https://my.tri.co.id/data'
-
-driver.get(main_url)
-
-time.sleep(5)
-products_button = driver.find_elements(By.CLASS_NAME, 'card-body')
-for j in range(2, len(products_button), 2):
-    while True:
-        try:
-            products_button[j].click()
-            break
-        except:
-            products_button = driver.find_elements(By.CLASS_NAME, 'card-body')
+def scrape_data(button_index):
+    driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
+    main_url = 'https://my.tri.co.id/data'
+    driver.get(main_url)
 
     time.sleep(5)
+    _ = driver.find_elements(By.CLASS_NAME, 'card-body')[2::2][button_index].click()
+
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'card-more-btn')))
     buttons = driver.find_elements(By.CLASS_NAME, 'card-more-btn')
-    for i in range(len(buttons)):
+
+    products_info = []
+    for i in range(4):
         while True:
             try:
                 buttons[i].click()
@@ -32,13 +31,28 @@ for j in range(2, len(products_button), 2):
             except:
                 buttons = driver.find_elements(By.CLASS_NAME, 'card-more-btn')
 
-        time.sleep(5)
+        time.sleep(3)
         infos = driver.find_elements(By.CLASS_NAME, 'card-body')
         try:
             for info in infos:
-                print(info.text)
+                if info.text != '':
+                    products_info.append(info.text.split('\n'))
         except:
-            print(infos.text)
+            products_info.append(infos.text.split('\n'))
         driver.back()
-        break
+            
+    return products_info
 
+def process_scraped_data(scraped_data):
+    for index, product in enumerate(scraped_data):
+        product.remove('Rincian Paket')
+        product.remove('Perpanjang otomatis')
+        product.remove('Ya')
+        product.remove('Beli')
+        scraped_data[index] = product
+ 
+    return scraped_data
+
+
+scraped_data = scrape_data(0)
+print(process_scraped_data(scraped_data))
--- scrapingFunctions/scrape_xl.py
+++ scrapingFunctions/scrape_xl.py
@@ -1,7 +1,53 @@
-import time
+import numpy as np
+import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = Options()
+options.add_argument("start-maximized")
+webdriver_path = './chromedriver'
+
+def scrape_data(url):
+    driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
+    driver.get(url)
+    width = 3000
+    height = 1000
+    driver.set_window_size(width, height)
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, 'slick-track')))
+    product_info = np.array(driver.find_element(By.CLASS_NAME, 'slick-track').text.split('\n'))
+    driver.close()
+
+    return product_info
+
+def process_scraped_data(scraped_data):
+    n_reshape = np.where(scraped_data == 'Beli Paket')[0][0] + 1
+    reshaped = scraped_data.reshape(-1, n_reshape)
+    final_scraped_data = []
+    for product in reshaped:
+        length = len(product)
+        if product[length-3][:2] == 'Rp':
+            cleaned =  np.delete(product, length-3)
+            final_scraped_data.append(cleaned)
+        else:
+            final_scraped_data.append(product)
+    
+    return np.array(final_scraped_data)
+
+def convert_scraped_data_to_csv(scraped_data):
+    data = pd.DataFrame()
+    for product in scraped_data:
+        temp_dict = {}
+        temp_dict['Product Name'] = [product[0]]
+        for benfefit_name, benefit in zip(product[1:-1:2], product[2:-1:2]):
+            temp_dict[benfefit_name] = [benefit]
+        temp_data = pd.DataFrame(temp_dict)
+        data = pd.concat([data, temp_data])
+
+    return data
+
 
 urls = [
     'https://www.xl.co.id/id/produk/paket-dan-addon/xtra-combo-flex-mc',
@@ -11,17 +57,11 @@ urls = [
     'https://www.xl.co.id/id/produk/paket-dan-addon/pilihan-xtra-kuota'
 ]
 
-options = Options()
-options.add_argument("start-maximized")
-webdriver_path = './chromedriver'
-
-driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
-store_info = []
+xl_products_data = pd.DataFrame()
 for url in urls:
-    driver.get(url)
-    width = 3000
-    height = 1000
-    driver.set_window_size(width, height)
-    time.sleep(10)
-    product_info = driver.find_element(By.CLASS_NAME, 'slick-track').text.split('\n')
-    print(product_info)
+    scraped_data = scrape_data(url)
+    final_scraped_data = process_scraped_data(scraped_data)
+    product_data = convert_scraped_data_to_csv(final_scraped_data)
+    xl_products_data = pd.concat([xl_products_data, product_data])
+
+xl_products_data.to_csv('XL_Products.csv', index=False)
